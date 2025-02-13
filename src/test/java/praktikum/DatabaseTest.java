1b88681256551717e9e3f1f3d2cbdaaf37aaf006
Ksusha Rusinova <rusinovakseniaolegovna@yandex.ru>
2025-02-13 23:54:14 +0400

    fix DatabaseTest and new jacoco

--- src/test/java/praktikum/DatabaseTest.java
+++ src/test/java/praktikum/DatabaseTest.java
@@ -1,14 +1,11 @@
 package praktikum;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
 public class DatabaseTest {
 
     @Test
